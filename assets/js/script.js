03202867b7c068cc9f312c79ebee30d18c0391b4
Jacob Seymour <jacobwseymour@gmail.com>
2023-01-30 18:47:38 +0000

    fixed issue of pulling from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,22 +36,15 @@ $(document).ready(function(){
     
     }
 
+    for(i = 0; i < localStorage.length; i++){
+		let time = localStorage.key(i);
+		let event = localStorage.getItem(time);
+		$(`[id='${time}']`).closest('.row').find('.content').val(event);
+	}
+
     hourUpdate();
 
     let interval = setInterval(hourUpdate, 15000);
-
-    $('#hour-7 .description').val(localStorage.getItem('hour-7'));
-    $('#hour-8 .description').val(localStorage.getItem('hour-8'));
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-    $('#hour-18 .description').val(localStorage.getItem('hour-18'));
 
     $('#currentDay').text(moment().format("ddd, hA"));
 });
